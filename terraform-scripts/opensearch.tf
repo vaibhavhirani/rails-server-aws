a2522e090e37e06464c5fb69a6a6c07a4c0b0450
vaibhavhirani <vaibhavhirani@gmail.com>
2023-08-19 23:43:04 +0530

    added opensearch into subnet

--- terraform-scripts/opensearch.tf
+++ terraform-scripts/opensearch.tf
@@ -9,7 +9,34 @@ resource "aws_opensearch_domain" "domain" {
     cloudwatch_log_group_arn = aws_cloudwatch_log_group.logs.arn
     log_type                 = "INDEX_SLOW_LOGS"
   }
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
   tags = {
     Domain = "Rail Server"
   }
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["es.amazonaws.com"]
+    }
+
+    actions = [
+      "logs:PutLogEvents",
+      "logs:PutLogEventsBatch",
+      "logs:CreateLogStream",
+    ]
+
+    resources = ["arn:aws:logs:*"]
+  }
+}
+resource "aws_cloudwatch_log_resource_policy" "cloudwatchpolicy" {
+  policy_name     = "cloudwatchpolicy"
+  policy_document = data.aws_iam_policy_document.policy.json
 }
