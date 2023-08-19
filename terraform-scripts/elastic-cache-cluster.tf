d561c79849aae215b65a88253745437583520c02
vaibhavhirani <vaibhavhirani@gmail.com>
2023-08-20 00:19:23 +0530

    redis concerning resources united

--- terraform-scripts/elastic-cache-cluster.tf
+++ terraform-scripts/elastic-cache-cluster.tf
@@ -1,3 +1,18 @@
+# VPC for Redis Cluster
+resource "aws_vpc" "rails_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Subnet for Redis Cluster
+resource "aws_subnet" "rails_subnet" {
+  vpc_id     = aws_vpc.rails_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "rails"
+  }
+}
+
 # Subnet Group for Redis Cluster
 resource "aws_elasticache_subnet_group" "redis" {
   name       = "redis-subnet"
